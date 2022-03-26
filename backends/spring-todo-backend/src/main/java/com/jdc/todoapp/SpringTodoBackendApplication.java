ba64f4253e675e16208f2d1baf04d8777907fc84
jhondevcode <jhondev.code@gmail.com>
2022-03-26 15:41:08 -0500

    Inicio backend todo app en spring boot

---
+++ backends/spring-todo-backend/src/main/java/com/jdc/todoapp/SpringTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoBackendApplication.class, args);
+	}
+
+}
---
+++ backends/spring-todo-backend/src/test/java/com/jdc/todoapp/SpringTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
